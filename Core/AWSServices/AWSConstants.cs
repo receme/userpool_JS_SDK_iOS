1199dfd2f3d04ab64ac0e983fc7042d0ab638199
receme <tahmid1989@gmail.com>
2016-08-15 23:50:11 +0600

    aws credentials removed

--- Core/AWSServices/AWSConstants.cs
+++ Core/AWSServices/AWSConstants.cs
@@ -3,9 +3,9 @@ namespace Core
 {
 	public class AWSConstants
 	{
-		public const string IDENTITY_POOL_ID = "us-east-1:98fb4e4b-c96d-4f5f-9917-13e6bb250e54";
-		public const string REGION = "us-west-2";
-		public const string USER_POOL_ID = "us-west-2_Og3WVNHh7";
+		public const string IDENTITY_POOL_ID = "<Your identity pool>";
+		public const string REGION = "<Region you have choosen>";
+		public const string USER_POOL_ID = "<Your user pool id>";
 
 		//user pool
 		public const string USER_POOL_PROVIDER = "cognito-idp." + REGION + ".amazonaws.com/" + USER_POOL_ID;
--- UserPool_JSsdk_IOS/www/js/config.js
+++ UserPool_JSsdk_IOS/www/js/config.js
@@ -1,6 +1,6 @@
 var config = {
-    region: "us-west-2",
-    identityPoolId: "us-west-2:2ab22adc-5ca6-42ec-9f5f-d608204038a0",
-    userPoolId: "us-west-2_Og3WVNHh7",
-    clientId: "6k48ndlg0o5q497873049dfkll"
+    region: "<REGION>",
+    identityPoolId: "<IDENTITY_POOL_ID>",
+    userPoolId: "<USER_POOL_ID>",
+    clientId: "<USER POOL APP CLIENT ID>"
 };
